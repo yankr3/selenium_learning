20063bf69f8f0cf64483f5c240a2fa04ea82df79
Yana <ermolova.yana@gmail.com>
2022-07-14 17:51:08 +0300

    add alert to main_page test

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,9 +6,10 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
